03c1b5980360512bced6d2fd1c7c6616c4a91d21
sk-rezwan <skrezwan12@gmail.com>
2019-11-07 10:54:17 +0600

    like and unlike for reply

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+// use App\Http\Controllers\LikeController;
+
 Route::apiResource('/question', 'QuestionController');
 Route::apiResource('/category', 'CategoryController');
 Route::apiResource('/question/{question}/reply', 'ReplyController');
+Route::post('/like/{reply}', 'LikeController@likeIt');
+Route::delete('/like/{reply}', 'LikeController@unlikeIt');
